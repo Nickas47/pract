21e1f09f1b64ebaa2d46a65e09d4484e174b5639
Nickas47 <nick-rasmus@mail.ru>
2017-09-18 23:02:35 +0300

    Add files via upload

--- chat.php
+++ chat.php
@@ -1,38 +1,37 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'messages') OR DIE('Помилка');
-if(isset($_POST['mess']) && $_POST['mess']!="" && $_POST['mess']!=" ")
-{
-//Стартуем сессию для записи логина пользователя
-session_start();
-//Принимаем переменную сообщения
-$mess=$_POST['mess'];
-//Переменная с логином пользователя
-$login=$_SESSION['login'];
-//Подключаемся к базе
-include("bd.php");
-//Добавляем все в таблицу
-$res=mysql_query("INSERT INTO `messages` (`login`,`message`) VALUES ('$login','$mess') ");
-}
-?>
+$dbc = mysqli_connect('localhost', 'root', '', 'lesson') OR DIE('Помилка');
+if (!dbc) {
+	echo "проблеми з базою";
+	}
+	$userMsg = $_POST['usermsg'];
+
+	$charSql ="INSERT INTO  `lesson`.`message` (`id` ,`mess`)VALUES (NULL ,  '$userMsg')";
+	mysqli_query($dbc, $charSql);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Чат</title>
+<title>Chat - Customer Module</title>
 <link type="text/css" rel="stylesheet" href="css/style_chat.css" />
 </head>
  
 <div id="wrapper">
     <div id="menu">
-        <p class="welcome">Привіт, <b><?php echo $_COOKIE['name'];?></b></p>
-        <p class="logout"><a id="exit" href="index_stud.php">Вийти</a></p>
+        <p class="welcome">Welcome, <b><?php echo $_COOKIE['name']; ?></b></p>
+        <p class="logout"><a id="exit" href="index_stud.php">Exit Chat</a></p>
         <div style="clear:both"></div>
     </div>
-     
-    <div id="chatbox"></div>
-     
-    <form name="message" action="">
-        <input name="usermsg" type="text" id="usermsg" size="63" />
+    <div id="chatbox"> 
+    <?php
+    $sql = "SELECT * FROM `message`";
+    $result = mysqli_query($dbc, $sql);
+    while($row = mysqli_fetch_assoc($result)) {
+    	 echo $_COOKIE['name']. $row['mess'].'</br>';
+    }
+    ?>
+    </div>
+        <form name="messag" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input name="usermsg" type="text" id="usermsg" size="63" style="overflow: auto"/>
         <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
     </form>
 </div>
